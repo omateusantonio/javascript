773d198f0b7c4a59314d768ce519f871aaf9cf83
omateusantonio <65185600+omateusantonio@users.noreply.github.com>
2022-10-06 18:43:27 -0300

    que tistreza

--- exercícios/aula12ex/ex002/script.js
+++ exercícios/aula12ex/ex002/script.js
@@ -3,4 +3,8 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('ano')
     var res = document.getElementById('res')
+
+    if (ano - fano < 20) {
+        console.log('tudo certo')
+    }
 }
